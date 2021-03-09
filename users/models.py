c42148cb3c7aa10e89b6c2b2b5ab217f3c770063
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-03-09 16:11:29 +0000

    added; mechanism to standardize profile picture

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,3 +10,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    # override save method
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs) # inherit from parent method save
+
+        img = Image.open(self.image.path)
+
+        # resize thumbnail
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
