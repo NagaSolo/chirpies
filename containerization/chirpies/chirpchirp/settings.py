1eef0d081735fb8ff770df392b093e437faa158c
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-04-08 04:26:33 +0100

    modified; database setting to make use of postgresql

--- containerization/chirpies/chirpchirp/settings.py
+++ containerization/chirpies/chirpchirp/settings.py
@@ -80,8 +80,14 @@ WSGI_APPLICATION = 'chirpchirp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
